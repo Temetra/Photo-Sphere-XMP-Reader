8bb0b5b23da84aebf59525252f7308cf3995a04e
pauln <temetra.xy+git@gmail.com>
2018-09-17 20:15:04 +0100

    Added type parsing to getGPanoValue

--- src/XMPReader.js
+++ src/XMPReader.js
@@ -52,13 +52,66 @@ var _xmpreader;
 			}
 		}
 
-		// Returns string value of GPano XMP attribute
-		XmpReader.prototype.getGPanoValue = function(attribName) {
+		// Functions for converting GPano text values to type
+		function parseBoolean(text) {
+			let val = text.toLowerCase().trim();
+			if (val === "true") return true;
+			else if (val === "false") return false;
+			else return null;
+		}
+
+		function parseDate(text) {
+			return Date.parse(text);
+		}
+
+		var xmpParameterConversion = {
+			UsePanoramaViewer: parseBoolean,
+			//CaptureSoftware: [string],
+			//StitchingSoftware: [string],
+			//ProjectionType: [string],
+			PoseHeadingDegrees: parseFloat,
+			PosePitchDegrees: parseFloat,
+			PoseRollDegrees: parseFloat,
+			InitialViewHeadingDegrees: parseInt,
+			InitialViewPitchDegrees: parseInt,
+			InitialViewRollDegrees: parseInt,
+			InitialHorizontalFOVDegrees: parseFloat,
+			InitialVerticalFOVDegrees: parseFloat,
+			FirstPhotoDate: parseDate,
+			LastPhotoDate: parseDate,
+			SourcePhotosCount: parseInt,
+			ExposureLockUsed: parseBoolean,
+			CroppedAreaImageWidthPixels: parseInt,
+			CroppedAreaImageHeightPixels: parseInt,
+			FullPanoWidthPixels: parseInt,
+			FullPanoHeightPixels: parseInt,
+			CroppedAreaLeftPixels: parseInt,
+			CroppedAreaTopPixels: parseInt,
+			InitialCameraDolly: parseFloat
+		};
+
+		// If the GPano XMP attribute exists, returns value converting to type if possible
+		// Otherwise returns null
+		XmpReader.prototype.getGPanoValue = function (attribName) {
+			// Find the attribute
 			var fullname = "GPano:" + attribName + "=\"";
 			var start = this.XMPData.indexOf(fullname);
 			var end = this.XMPData.indexOf("\"", start + fullname.length);
-			if (start < 0 || end < 0) return "";
-			return this.XMPData.substring(start + fullname.length, end);
+
+			// No attribute found
+			if (start < 0 || end < 0) return null;
+
+			// Extract the attribute
+			var attribValue = this.XMPData.substring(start + fullname.length, end);
+
+			// Convert the attribute
+			if (attribName in xmpParameterConversion) {
+				var parse = xmpParameterConversion[attribName];
+				return parse(attribValue);
+			}
+
+			// Otherwise return the text
+			return attribValue;
 		};
 
 		// Extracts XMP data from file blob
